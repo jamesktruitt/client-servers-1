1f732d87689d48c0589bc910b1968d24c73b0471
James Truitt <tru@rockyagirl.com>
2017-12-12 10:33:47 -0700

    changes made

--- app.js
+++ app.js
@@ -1,34 +1,34 @@
-const express = require("express");
-const cors = require("cors");
-const data = require("./data/cohorts");
+const express = require('express');
+const cors = require('cors');
+const data = require('./data/cohorts');
 
 const app = express();
 app.use(cors());
 
-function findById(data, id){
-    for (let i = 0; i < data.length; i++){
-        if (data[i].id == id) {
-            return data[i];
-        }
+function findById(data, id) {
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].id == id) {
+      return data[i];
     }
-    return null;
+  }
+  return null;
 }
 
-app.get("/", function (request, response) {
-    response.json({data});
+app.get('/', (request, response) => {
+  response.json({ data });
 });
 
-app.get("/:id", function (request, response) {
-    var record = findById(data, request.params.id);
-    if (!record) {
-        response.status(404).json({
-            error: {
-                message: 'no record found!'
-            }
-        });
-    } else {
-        response.json({data: record});
-    }
+app.get('/:id', (request, response) => {
+  const record = findById(data, request.params.id);
+  if (!record) {
+    response.status(404).json({
+      error: {
+        message: 'no record found!'
+      }
+    });
+  } else {
+    response.json({ data: record });
+  }
 });
 
-app.listen(process.env.PORT||3000);
+app.listen(process.env.PORT || 3000);
